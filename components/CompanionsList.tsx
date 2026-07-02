31b0ff1e41e883295d225dd9806f8b41bdd45bd7
srujannallapu-123 <srujannallapu123@gmail.com>
2026-07-02 17:03:57 +0100

    add companion support and related links

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -7,7 +7,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { cn } from "@/lib/utils";
+import { cn, getSubjectColor } from "@/lib/utils";
 import { Companion } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
@@ -38,16 +38,43 @@ const CompanionsList = ({title, companions, classNames}: CompanionsListProps) =>
         <TableRow key={id}>
           <TableCell>
             <Link href={`/companions/${id}`} >
-              <div>
-                <div>
+              <div className="flex items-center gap-2">
+                <div className="size-\[72px\] flex justify-center items-center rounded-lg max-md:hidden" style={{backgroundColor: getSubjectColor(subject)}}>
                   <Image src={`/icons/${subject}.svg`}
                    alt={subject}
                    width={35}
                    height={35}
                      />
                 </div>
+                <div className="flex flex-col gap-2">
+                  <p className="font-bold text-2xl">{name}</p>
+                  <p className="text-lg">{topic}</p>
+                </div>
               </div>
             </Link>
+          </TableCell>
+
+          <TableCell>
+            <div className="subject-badge w-fit max-md:hidden">
+              {subject}
+            </div>
+            <div className="flex justify-center items-center rounded-lg w-fit p-2 md:hidden" style={{backgroundColor:getSubjectColor(subject)}} >
+                  <Image src={`/icons/${subject}.svg`}
+                   alt="subject"
+                   width={18}
+                   height={18}  />
+            </div>
+          </TableCell>
+
+          <TableCell>
+              <div className="flex items-center justify-center gap-2 w-full">
+                <p className="text-2xl">{duration} {' '}
+                   <span className="max-md:hidden">mins</span></p>
+
+                 <Image src={"/icons/clock.svg"} alt="clock"
+                    width={14}
+                    height={14} className="md:hidden"/>  
+              </div>
           </TableCell>
         </TableRow>
       ))}
