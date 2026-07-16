5e7686609561ca88abf4c0c9fc1f08bbfd00644c
srujannallapu-123 <srujannallapu123@gmail.com>
2026-07-16 17:27:42 +0100

    searchfilter updated

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,40 +1,66 @@
-'use client'
+"use client";
 
 import Image from "next/image";
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { useState } from "react";
-
-
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  formUrlQuery,
+  removeKeysFromUrlQuery,
+} from "@jsmastery/utils";
 
 const SearchInput = () => {
-
   const pathname = usePathname();
-  
   const router = useRouter();
-
   const searchParams = useSearchParams();
+  const query = searchParams.get("topic");
 
-  const query = searchParams.get('topic') || '';
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const [searchQuery, setSearchQuery] = useState('')
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        if (searchQuery === query) return;
 
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "topic",
+          value: searchQuery,
+        });
 
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === "/companions" && query) {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["topic"],
+          });
 
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, query, router, pathname, searchParams]);
 
   return (
-    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+    <div className="relative flex h-fit items-center gap-2 rounded-lg border border-black px-2 py-1">
+      <Image
+        src="/icons/search.svg"
+        alt="Search"
+        width={15}
+        height={15}
+      />
 
-           <Image src={"/icons/search.svg"} alt="search" width={15} height={15} />
-
-           <input 
-              placeholder="Search Companions..."
-              className="outline-none"
-              value={searchQuery}
-              onChange={(e)=>{e.target.value}}
-            />
-
+      <input
+        type="search"
+        placeholder="Search companions..."
+        className="outline-none"
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
