d97ff079103ce3e173cebe81b0e9520657d8ab17
srujannallapu-123 <srujannallapu123@gmail.com>
2026-07-24 18:06:20 +0100

    AI Companion setup

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,8 +1,14 @@
 'use client'
 
 import { cn, getSubjectColor } from "@/lib/utils"
+import { vapi } from "@/lib/vapi.sdk"
 import { CompanionComponentProps } from "@/types"
-import { useState } from "react"
+import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
+
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+
+import soundwaves from '@/constants/soundwaves.json'
 
 
 enum CallStatus {
@@ -20,6 +26,57 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
 
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
 
+    const [isSpeaking, setIsSpeaking] = useState(false)
+
+    const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+    useEffect(()=>{
+
+      if(lottieRef){
+        if(isSpeaking){
+          lottieRef.current?.play()
+        }else{
+          lottieRef.current?.stop()
+        }
+      }
+
+    },[isSpeaking, lottieRef])
+
+    useEffect(()=>{
+       
+      const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+
+      const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+
+      const onMessage = () =>{}
+
+      const onSpeechStart = () => setIsSpeaking(true)
+      const onSpeechEnd = () => setIsSpeaking(false)
+
+
+
+      const onError = (error:Error) => console.log('Error', error)
+
+
+      vapi.on('call-start', onCallStart)
+      vapi.on('call-end', onCallEnd)
+      vapi.on('message', onMessage)
+      vapi.on('speech-start', onSpeechStart)
+      vapi.on('speech-end', onSpeechEnd)
+      
+      return () => {
+
+      vapi.off('call-start', onCallStart)
+      vapi.off('call-end', onCallEnd)
+      vapi.off('message', onMessage)
+      vapi.off('speech-start', onSpeechStart)
+      vapi.off('speech-end', onSpeechEnd)
+
+      }
+
+
+    },[])
+
   return (
     <section className="flex flex-col h-[70vh]">
       <section className="flex gap-8 max-sm:flex-col">
@@ -27,9 +84,33 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
             <div className="companion-avatar"
              style={{backgroundColor:getSubjectColor(subject)}}>
 
-             <div className={cn( 'absolute transition-opacity duration-1000')}>
+             <div className={cn( 'absolute transition-opacity duration-1000',
+               callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ? 'opacity-1001' : 'opacity-0', callStatus === CallStatus.CONNECTING && 'opacity-100 animate-pulse' )}>
+              
+              <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className="max-sm:w-fit" />
+
+             </div>
+
+             <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.ACTIVE ? 'opacity-100' : 'opacity-0')}>
+                 
+                 <Lottie 
+                  lottieRef={lottieRef}
+                  animationData={soundwaves}
+                  autoplay={false}
+                  className="companion-lottie"
+                 
+                 />
              </div>   
             </div>
+            <p className="font-bold text-2xl">{name}</p>
+
+        </div>
+
+        <div className="user-section">
+          <div className="user-avatar">
+            <Image src={userImage} alt={userName} width={130} height={130} className="rounded-lg" />
+
+          </div>
 
         </div>
 
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -28,11 +28,11 @@ export const getAllCompanions = async({limit = 10, page=1, subject, topic}: GetA
 
     if(subject && topic){
        query = query.ilike('subject',`%${subject}%`)
-         .or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+         .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
     }else if(subject){
         query = query.ilike('subject',`%${subject}%`)
     }else if(topic){
-       query = query.or(`topic.ilike.%${topic}%, name.ilike.%${topic}%`)
+       query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
     }
 
 
