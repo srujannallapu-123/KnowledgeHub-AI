75b09b9c13534b77d0101f0813e04bb411891b3e
srujannallapu-123 <srujannallapu123@gmail.com>
2026-07-30 11:06:16 +0100

    Fix dynamic homepage rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,43 @@
-import CompanionCard from '@/components/CompanionCard'
-import CompanionsList from '@/components/CompanionsList'
-import CTA from '@/components/CTA'
-import { Button } from '@/components/ui/button'
-import { recentSessions } from '@/constants'
-import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
-import { getSubjectColor } from '@/lib/utils'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
+export const dynamic = "force-dynamic";
 
-const Page = async() => {
-
-   const companions = await getAllCompanions({limit:3});
-   const recentSessionsCompanions = await getRecentSessions(10);
-
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
 
   return (
     <main>
-      <h1 className='text-2xl underline'>Popular Companions</h1>
-       <section className='home-section'>
-        
-        {companions.map((companion) =>(
+      <h1 className="text-2xl underline">Popular Companions</h1>
+
+      <section className="home-section">
+        {companions.map((companion) => (
           <CompanionCard
-           key={companion.id}
-           {...companion}
-           color={getSubjectColor(companion.subject)}
-           />
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
         ))}
-        
-       </section>
+      </section>
 
-       <section className='home-section'>
-        <CompanionsList 
-         title="Recently completed sessions"
-         companions={recentSessionsCompanions}
-         classNames="w-2/3 max-lg:w-full"
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessionsCompanions}
+          classNames="w-2/3 max-lg:w-full"
         />
-        <CTA/>
-       </section>
-    </main>
-  )
-}
 
-export default Page
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
